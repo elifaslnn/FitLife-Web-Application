308de19256a909f2fba6d664058de665e82ab43d
StrayRaider <41emrekaya919@gmail.com>
2023-11-11 16:07:17 +0300

    tablo silme ve oluşturma kodu eklendi

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import pg from 'pg'
  
 const postgresConnection = new pg.Pool({
-  host: 'fitlife.covtsuyb4can.eu-north-1.rds.amazonaws.com',
+  host: 'database-1.cf1n4shvkpyh.us-east-1.rds.amazonaws.com',
   user: 'postgres',
   password:'210201eE',
   port:5432,
@@ -9,6 +9,7 @@ const postgresConnection = new pg.Pool({
     rejectUnauthorized: false
 }
 })
+
 postgresConnection.connect(error => {
     if(error){
         console.log("no connection , error ",error.stack)
@@ -17,6 +18,22 @@ postgresConnection.connect(error => {
     }
 })
 
+function run_sql(sql_command){
+    postgresConnection.query(sql_command, function(err, result){
+        if(err){
+            console.log('error: ', err);
+            process.exit(1);
+        }else{
+            console.log("command run sqccesfully")
+        }
+        process.exit(0);
+    });
+}
+
+run_sql("CREATE TABLE IF NOT EXISTS login ();")
+run_sql("DROP TABLE IF EXISTS login;")
+run_sql("DROP TABLE IF EXISTS LogIn;")
+run_sql("DROP TABLE IF EXISTS users;")
 
 
 console.log("merhaba emre <3");
