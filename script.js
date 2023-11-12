6f23a8fa2b17449d0a2eb2b62b9bf55e4d98b7cb
Elif Aslan <elfaslnvon@gmail.com>
2023-11-12 19:42:48 +0300

    node.js, html bağlantısı yapıldı

--- script.js
+++ script.js
@@ -1,43 +1,60 @@
-import pg from 'pg'
- 
+import pg from "pg";
+import express from "express";
+import path from "path";
+import * as url from "url";
+
 const postgresConnection = new pg.Pool({
-  host: 'database-1.cf1n4shvkpyh.us-east-1.rds.amazonaws.com',
-  user: 'postgres',
-  password:'210201eE',
-  port:5432,
+  host: "database-1.cf1n4shvkpyh.us-east-1.rds.amazonaws.com",
+  user: "postgres",
+  password: "210201eE",
+  port: 5432,
   ssl: {
-    rejectUnauthorized: false
-}
-})
+    rejectUnauthorized: false,
+  },
+});
+
+//html bağlantısı
+
+/*const express = require("express");
+const path = require("path");*/
+
+const app = express();
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+const publicPath = path.join(__dirname, "public");
+
+app.use(express.static(publicPath));
+app.listen(5000);
+
 // postgresql amazon servisindeki konuma bağlanan kısım
-postgresConnection.connect(error => {
-    if(error){
-        console.log("no connection , error ",error.stack)
-    }else{
-        console.log("success")
-    }
-})
+postgresConnection.connect((error) => {
+  if (error) {
+    console.log("no connection , error ", error.stack);
+  } else {
+    console.log("success");
+  }
+});
 
 // parametre almadan sadece basit bir sql sorgusu çalışıtran fonksiyon
 // örnek tablo oluştur sil vs.
-function run_sql(sql_command){
-    postgresConnection.query(sql_command, function(err, result){
-        if(err){
-            console.log('error at run_sql : ', err);
-        }else{
-            console.log("command run succesfully")
-        }
-    });
+function run_sql(sql_command) {
+  postgresConnection.query(sql_command, function (err, result) {
+    if (err) {
+      console.log("error at run_sql : ", err);
+    } else {
+      console.log("command run succesfully");
+    }
+  });
 }
 
 //run_sql("DROP TABLE login;")
 
 // create login table
-run_sql("CREATE TABLE IF NOT EXISTS login ( \
+/*run_sql(
+  "CREATE TABLE IF NOT EXISTS login ( \
     user_no INT PRIMARY KEY, \
     mail TEXT, \
-    password TEXT);")
-
+    password TEXT);"
+);*/
 
 //run_sql("create table users (mail text primary key,    password text, name text,surname text,  role text , birth_date date, gender text, phone_number INT,    photo text);
 
@@ -60,22 +77,23 @@ run_sql("CREATE TABLE IF NOT EXISTS login ( \
 //create table users_progress (id INT primary key, mail text, weight INT, height INT, fat_rate INT, muscle_mass INT, body_mass_index INT);
 
 //create user
-let user_no = 1
-let mail = "sonemre41@gmail.com"
-let password = 1234
+let user_no = 1;
+let mail = "sonemre41@gmail.com";
+let password = 1234;
 
 //create new login
-run_sql(`INSERT INTO login VALUES ( '${user_no}','${mail}','${password}');`)
+//run_sql(`INSERT INTO login VALUES ( '${user_no}','${mail}','${password}');`);
 
-const rows = await postgresConnection.query('SELECT * FROM login');
+/*const rows = await postgresConnection.query("SELECT * FROM login");
 
 // bir obje döndürür aradığımız tüm data burada
-console.log("rows are here : ",rows )
+console.log("rows are here : ", rows);
 // tüm satırları bir list halinde döndürür
-console.log("rows are here : ",rows.rows )
+console.log("rows are here : ", rows.rows);
 // ilk satırdaki diction verisini döndürür
-console.log("rows are here : ",rows.rows[0] )
+console.log("rows are here : ", rows.rows[0]);
 // dictiondaki herhangi bir obje çekilmesi böyle yapılır
-console.log("rows are here : ",rows.rows[0].user_no)
+console.log("rows are here : ", rows.rows[0].user_no);
 
 console.log("merhaba emre <3");
+*/
