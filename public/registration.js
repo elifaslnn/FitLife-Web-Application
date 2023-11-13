7a43603e8f84f2af90f2c8056ff3c10c5279ac56
Elif Aslan <elfaslnvon@gmail.com>
2023-11-13 16:41:43 +0300

    login ve signin frontendleri yazıldı

--- public/registration.js
+++ public/registration.js
@@ -1,11 +1,11 @@
 function visible_passwd() {
-    var x = document.getElementById("password_visible");
-    if (x.type === "password") {
-      x.type = "text";
-    } else {
-      x.type = "password";
-    }
-  } 
-function get_users_data(){
-    pass
+  var x = document.getElementById("password_visible");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+function get_users_data() {
+  pass;
 }
